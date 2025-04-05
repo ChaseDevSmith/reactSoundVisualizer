aff24467951c552e6767ed87f4433f94d0fcc190
ChaseDevSmith <50593929+ChaseDevSmith@users.noreply.github.com>
2025-04-05 16:01:18 -0500

    the payload

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import './App.css'
+import StationSelector from './stationSelector'
+import AudioVisualizer from './AudioVisualizer'
+function App() {
+  const [sound, setSound] = useState(null);
+  const [currentStation, setStation] = useState("BASIC");
+  const handleStationChange =(station)=> {
+
+    setStation(station);
+    console.log("changed station! EZZZ", station)
+  }
+  const handleFileChange = (e) =>{
+    const file = e.target.files[0]
+    if(file){
+      setSound(URL.createObjectURL(file))
+    }
+  }
+
+// initAudio()
+  return (
+   <div id ="container">
+    <div id = "selectBar">
+    <StationSelector onStationChange={handleStationChange}/>
+    <input type="file"  accept="audio/*" onChange={handleFileChange}></input>
+    </div>
+    <AudioVisualizer audioFile={sound} station={currentStation}/>
+
+  
+   
+
+
+  </div>
+     
+ 
+
+  )
+}
+
+export default App
